436ba9e1881e8c172c7e4184e2e71f596114abe2
Cristian Racedo <CristianDRacedo@hotmail.com>
2022-06-27 10:15:46 -0300

    Refactorizacion de funciones

--- scripts/main.js
+++ scripts/main.js
@@ -6,17 +6,20 @@
     const mapFrm = d.querySelector('.map iframe');
 
 // Navegacion
-    menuBtn.onclick = () => {
+    menuBtn.onclick = toggleMenu()
+
+    const toggleMenu = () => {
         menuLst.classList.toggle('active') ?
         menuBtn.innerHTML = '&times;' :
         menuBtn.innerHTML = '&equiv;'
     }
-
 // Mapa de Contacto
-    mapBtn.addEventListener('click', () => {
+    mapBtn.addEventListener('click', () => toggleMap() )
+
+    function toggleMap(){
         if(mapFrm.classList.toggle('active')){
             mapBtn.classList.replace('icon-max', 'icon-min')
         } else {
             mapBtn.classList.replace('icon-min', 'icon-max')
         }
-    }   )
+    }
